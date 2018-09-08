d0a87b89119ff1fd3d2dbfeb710ec31310faf869
bao <baosws@gmail.com>
2018-09-08 10:19:08 +0700

    update tuple shorter using inheritance

--- lib/Tuple.h
+++ lib/Tuple.h
@@ -1,44 +1,23 @@
 #pragma once
 #include <tuple>
 #include <mutex>
-template <class... T>
-class Tuple {
-    std::tuple<T...> data;
-    std::mutex mtx;
-public:
-    // constructors
-    Tuple() {}
-    Tuple(T... args) : data(std::forward<T>(args)...) {}
-    Tuple(const Tuple&) = default;
-    Tuple(Tuple&& p) {
-        data.operator=(std::forward<std::tuple<T...>>(p));
-    }
-    Tuple(const std::tuple<T...>& p) {
-        data = p;
-    }
-    Tuple(std::tuple<T...>&& p) : data(std::forward<std::tuple<T...>>(p)) {}
-    
-    // copy operators
-    Tuple& operator=(const std::tuple<T...>& p) {
-        std::lock_guard lock(mtx);
-        data = p;
-        return *this;
-    }
-    Tuple& operator=(std::tuple<T...>&& p) {
-        std::lock_guard lock(mtx);
-        data.operator=(std::forward<std::tuple<T...>>(p));
-        return *this;
-    }
 
+template <class... T>
+class Tuple : public std::tuple<T...> {
+    std::tuple<T...>& __this = *this;
+public:
+    using std::tuple<T...>::tuple;
+    Tuple(const std::tuple<T...>& other) : std::tuple<T...>(other) {}
+    Tuple(std::tuple<T...>&& other) : std::tuple<T...>(std::move(other)) {}
+    
     // access method
     template <size_t i>
-    decltype(std::get<i>(data)) get() {
-        std::lock_guard lock(mtx);
-        return std::get<i>(data);
+    decltype(std::get<i>(__this)) get() {
+        return std::get<i>(__this);
     }
+    
     template <size_t i>
-    const decltype(std::get<i>(data)) get() const {
-        std::lock_guard lock(mtx);
-        return std::get<i>(data);
+    const decltype(std::get<i>(__this)) get() const {
+        return std::get<i>(__this);
     }
 };
