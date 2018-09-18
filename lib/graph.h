35f36b20ef36887034eb022ee410e4be323580fc
bao <baosws@gmail.com>
2018-09-19 03:21:09 +0700

    updated graph with adj_graph & edge_graph

--- lib/graph.h
+++ lib/graph.h
@@ -1,19 +1,82 @@
 #pragma once
 #include <vector>
 
-template <typename graph_type = std::vector<std::vector<int>>>
+template <bool bidirect = true>
 struct Graph {
-    graph_type adj;
-    std::vector<int> par;
-    Graph(int n): adj(n), par(n) {}
+    int _n = 0;
+    
+    template <typename W = int>
+    struct Node {
+        typedef W weight_type;
+        int tar;
+        W w;
+        Node(int tar, W w = 0) : tar(tar), w(w) {}
+    };
 
-    template <bool bidirect = true>
-    void add_edge(int u, int v) {
-        if (u != v) {
-            adj[u].push_back(v);
-#if bidirect
-            adj[v].push_back(u);
-#endif
+    template <typename W = int>
+    struct Edge {
+        typedef W weight_type;
+        int src, tar;
+        W w;
+        Edge(int src, int tar, W w = 0): src(src), tar(tar), w(w) {}
+    };
+
+    virtual size_t n() const {
+        return _n;
+    }
+};
+
+template <bool Bi = true,
+          typename Node = Graph<>::Node<>,
+          typename Adj = std::vector<Node>>
+struct AdjGraph: virtual Graph<Bi> {
+    std::vector<Adj> adj;
+
+    typedef Node node_type;
+    typedef Adj adj_type;
+    static constexpr bool is_bidirected = Bi;
+    
+    AdjGraph(size_t n): adj(n) {
+        this->_n = n;
+    }
+    
+    virtual void add_edge(int u, int v, typename node_type::weight_type w = 0) {
+        adj[u].emplace_back(v, w);
+        if (Bi) {
+            adj[v].emplace_back(u, w);
         }
+    }
+
+    Adj& operator[](int i) {
+        return adj[i];
+    }
+};
+
+template <bool Bi = true,
+          typename Edge = Graph<>::Edge<>>
+class EdgeGraph: virtual Graph<Bi> {
+protected:
+    std::vector<Edge> edges;
+
+public:
+    typedef Edge edge_type;
+    static constexpr bool is_bidirected = Bi;
+
+    EdgeGraph(size_t n) {
+        this->_n = n;
+    }
+    
+    virtual void add_edge(int u, int v, typename edge_type::weight_type w = 0) {
+        edges.emplace_back(u, v, w);
+        if (Bi) {
+            edges.emplace_back(v, u, w);
+        }
+    }
+
+    typename std::vector<Edge>::const_iterator begin() const {
+        return edges.begin();
+    }
+    typename std::vector<Edge>::const_iterator end() const {
+        return edges.end();
     }
 };
